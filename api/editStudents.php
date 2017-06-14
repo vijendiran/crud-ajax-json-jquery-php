8f586a32aefee224a323c5d3da7d94cd64559341
vijendiran <vijendiran95@gmail.com>
2017-06-14 18:38:55 +0530

    added php file

--- api/editStudents.php
+++ api/editStudents.php
@@ -1,7 +1,5 @@
-<?php
-
-$dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","root");
-$sql = " INSERT INTO schoolstudents(name,fathername,rollno,degree,branch) VALUES (:name,:fathername,:rollno,:degree,:branch)";
+<?php $dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","root");
+$sql = "UPDATE schoolstudents(name,fathername,rollno,degree,branch) VALUES (:name,:fathername,:rollno,:degree,:branch)";
 $addStudentsQuery = $dbh->prepare($sql);
 $addStudentsQuery->bindParam(":name",$_POST["name"],PDO::PARAM_STR);
 $addStudentsQuery->bindParam(":fathername",$_POST["fathername"],PDO::PARAM_STR);
