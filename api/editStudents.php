d251e31904651ac7285d891d28ec0601b0a9654c
vijendiran <vijendiran95@gmail.com>
2017-06-15 12:16:08 +0530

    code is being processed

--- api/editStudents.php
+++ api/editStudents.php
@@ -1,5 +1,9 @@
-<?php $dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","root");
-$sql = "UPDATE schoolstudents(name,fathername,rollno,degree,branch) VALUES (:name,:fathername,:rollno,:degree,:branch)";
+<?php
+
+
+$id = $_POST["sid"];
+$dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","root");
+$sql = "UPDATE schoolstudents SET (name = :name,fathername = :fathername,rollno = :rollno,degree = :degree,branch = :branch) WHERE id=$id";
 $addStudentsQuery = $dbh->prepare($sql);
 $addStudentsQuery->bindParam(":name",$_POST["name"],PDO::PARAM_STR);
 $addStudentsQuery->bindParam(":fathername",$_POST["fathername"],PDO::PARAM_STR);
--- studentapp.js
+++ studentapp.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
+  var studentsList;
+  function getall() {
+    $(".studentsList").html("");
+    $.ajax({
+      url:"http://localhost/crud-ajax-json-jquery-php/api/getStudents.php",
+      method:"GET",
+      success:function(data) {
+        console.log(data);
+        studentsList = JSON.parse(data);
+        for (var i = 0; i < studentsList.length; i++) {
+          console.log(studentsList[i]["name"]);
+          $(".studentsList").append("<li>"+studentsList[i]["name"]+"</li>");
+        }
+        $("body").on("click",".studentsList li",function() {
+          var index = $(this).index();
 
+          $(".Edit-form .studentName").val(studentsList[index]["name"]);
+          $(".Edit-form .studentFname").val(studentsList[index]["fathername"]);
+          $(".Edit-form .studentRollno").val(studentsList[index]["rollno"]);
+          $(".Edit-form .studentDegree").val(studentsList[index]["degree"]);
+          $(".Edit-form .studentBranch").val(studentsList[index]["branch"]);
+          $(".Edit-form .sid").val(studentsList[index]["id"]);
+
+
+          // console.log("li works");
+          // console.log($(this).index());
+          // console.log(studentsList[0]);
+        });
+      }
+
+    })
+
+  }
+  getall();
 $.ajax({
   url:"http://localhost/crud-ajax-json-jquery-php/api/getStudents.php",
   method:"GET",
@@ -8,10 +41,59 @@ $.ajax({
     var studentsList = JSON.parse(data);
     for (var i = 0; i < studentsList.length; i++) {
       console.log(studentsList[i]["name"]);
-      $(".studentsList").append("<li>"+studentsList[i]["name"]+"</li>");
     }
+    $("body").on("click",".studentsList li",function() {
+      var index = $(this).index();
+      $(".Edit-form .studentName").val(studentsList[index]["name"]);
+      $(".Edit-form .studentFname").val(studentsList[index]["fathername"]);
+      $(".Edit-form .studentRollno").val(studentsList[index]["rollno"]);
+      $(".Edit-form .studentDegree").val(studentsList[index]["degree"]);
+      $(".Edit-form .studentBranch").val(studentsList[index]["branch"]);
+      $(".Edit-form .sid").val(studentsList[index]["id"]);
+
+
+      // console.log("li works");
+      // console.log($(this).index());
+      // console.log(studentsList[0]);
+    });
   }
+
 })
+//Edit-form
+$(".edit-student").click(function() {
+  var sName = $(".Edit-form .studentName").val();
+  var sFname = $(".Edit-form .studentFname").val();
+  var sRollno = $(".Edit-form .studentRollno").val();
+  var sDegree = $(".Edit-form .studentDegree").val();
+  var sBranch = $(".Edit-form .studentBranch").val();
+  var sID = $(".Edit-form .sid").val();
+  //
+  // console.log(sName);
+  // console.log(sFname);
+  // console.log(sRollno);
+  // console.log(sDegree);
+  // console.log(sBranch);
+  // console.log(sID);
+
+  $.ajax({
+    url:"http://localhost/crud-ajax-json-jquery-php/api/editStudents.php",
+    method:"POST",
+    data:{
+      sID : sid,
+      name : sName,
+      fathername : sFname,
+      rollno : sRollno,
+      degree : sDegree,
+      branch : sBranch
+    },
+    success:function(data) {
+    console.log(data);
+    // $(".studentsList").append("<li>"+sName+"</li>");
+
+  }
+  })
+})
+
 $(".submit-student").click(function() {
   var sName = $(".studentName").val();
   var sFname = $(".studentFname").val();
@@ -41,4 +123,5 @@ $(".submit-student").click(function() {
   }
   })
 })
+
 });
