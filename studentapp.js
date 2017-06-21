be5aa5e184a317661ef610380ed13cdfb30a5a64
vijendiran <vijendiran95@gmail.com>
2017-06-21 18:45:15 +0530

    changed append elements

--- studentapp.js
+++ studentapp.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         studentsList = JSON.parse(data);
         for (var i = 0; i < studentsList.length; i++) {
           console.log(studentsList[i]["name"]);
-          $(".studentsList").append("<li>"+studentsList[i]["name"]+"</li>");
+          $(".studentsList").append('<li><h2>'+studentsList[i]["name"]+'</h2><h3>'+studentsList[i]["fathername"]+'</h3><h4>'+studentsList[i]["rollno"]+'</h4><h5>'+studentsList[i]["degree"]+'</h5><h6>'+studentsList[i]["branch"]+'</h6></li>');
         }
 
       }
