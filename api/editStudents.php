94470f6ac465c4652b37ebd49443fca29bef53e2
vijendiran <vijendiran95@gmail.com>
2017-06-22 11:09:54 +0530

    removed unwanted php code

--- api/editStudents.php
+++ api/editStudents.php
@@ -1,12 +1,4 @@
 <?php
-// echo "string";
-// echo $_POST["sid"];
-// echo $_POST["name"];
-// echo $_POST["fathername"];
-// echo $_POST["rollno"];
-// echo $_POST["degree"];
-// echo $_POST["branch"];
-
 $id = $_POST["sid"];
 $dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","root");
 $sql = "UPDATE schoolstudents SET name = :name,fathername = :fathername,rollno = :rollno,degree = :degree,branch = :branch WHERE id = $id";
@@ -17,4 +9,4 @@ $addStudentsQuery->bindParam(":rollno",$_POST["rollno"],PDO::PARAM_STR);
 $addStudentsQuery->bindParam(":degree",$_POST["degree"],PDO::PARAM_STR);
 $addStudentsQuery->bindParam(":branch",$_POST["branch"],PDO::PARAM_STR);
 $addStudentsQuery->execute();
- ?>
+?>
